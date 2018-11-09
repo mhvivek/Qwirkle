735541f724bb4ae6e72481149bd851158d3b76f5
vivek <mittalhenklev@go.oes.edu>
2018-11-08 21:19:36 -0800

    Find all playable spots next to tiles

--- Board.py
+++ Board.py
@@ -1,10 +1,23 @@
 
 class Board:
-    tiles_on_board = {}
+    def __init__(self):
+        self.tiles_on_board = {}
+
     def add_to_board(self, tiles_to_play):
         for key, value in tiles_to_play.items():
             self.tiles_on_board[key] = value
         print(self.tiles_on_board)
 
-
-
+    def find_legal_plays(self):
+        possible_plays = set()
+        for coordinates in self.tiles_on_board:
+            x, y = coordinates.split(" ")
+            possible_plays.add((int(x) + 1, int(y)))
+            possible_plays.add((int(x) - 1, int(y)))
+            possible_plays.add((int(x), int(y) + 1))
+            possible_plays.add((int(x), int(y) - 1))
+        print(possible_plays)
+        for coordinates in self.tiles_on_board:
+            x, y = coordinates.split(" ")
+            possible_plays.remove((int(x), int(y)))
+        print(possible_plays)
--- Test/addToBoardTest.py
+++ Test/addToBoardTest.py
@@ -12,3 +12,9 @@ def test_play():
     board1.add_to_board(removed_from_hand)
     assert len(board1.tiles_on_board) == 1
 
+def test_coordinate_retrieval():
+    board2 = Board()
+    tile = Tile(Shape.CLOVER, Color.BLUE)
+    board2.add_to_board({"3 8": tile, "3 7": tile})
+    board2.find_legal_plays()
+
