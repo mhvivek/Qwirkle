618b8b20e1051b7412f2d821eff301e7d58fff4a
vivek <mittalhenklev@go.oes.edu>
2018-11-21 22:18:45 -0800

    find all tiles that can be played next to any tile based on all tiles in lines surrounding it

--- Board.py
+++ Board.py
@@ -11,7 +11,7 @@ class Board:
             self.tiles_on_board[key] = value
         print(self.tiles_on_board)
 
-    def find_legal_plays(self):
+    def find_spots(self):
         possible_plays = set()
         for coordinates in self.tiles_on_board:
             x, y = coordinates.split(" ")
@@ -19,10 +19,11 @@ class Board:
             possible_plays.add((int(x) - 1, int(y)))
             possible_plays.add((int(x), int(y) + 1))
             possible_plays.add((int(x), int(y) - 1))
-        print(possible_plays)
+        #print(possible_plays)
         for coordinates in self.tiles_on_board:
             x, y = coordinates.split(" ")
-            possible_plays.remove((int(x), int(y)))
+            if ((int(x), int(y))) in possible_plays:
+                possible_plays.remove((int(x), int(y)))
         print(possible_plays)
         return possible_plays
 
@@ -39,8 +40,53 @@ class Board:
             if color != tile_color:
                 tile1 = Tile(tile_shape, color)
                 similar_tiles.append(tile1)
-        print(*similar_tiles, sep='\n')
+        #print(*similar_tiles, sep='\n')
         return similar_tiles
+
+    def find_legal_plays(self):
+        spots = self.find_spots()
+        for spot in spots:
+            x = spot[0]
+            y = spot[1]
+            num = 2
+            plays_shortlist = []
+            if str(x) + " " + str(y + 1) in self.tiles_on_board:
+                tile = self.tiles_on_board[str(x) + " " + str(y + 1)]
+                possible_tiles = self.find_similar_tiles(self.tiles_on_board[str(x) + " " + str(y + 1)])
+                #print(*possible_tiles, sep='\n')
+                while str(x) + " " + str(y + num) in self.tiles_on_board:
+
+                    if self.tiles_on_board[str(x) + " " + str(y + num)].color == tile.color:
+                        for tiles in possible_tiles:
+                            if tiles.color == tile.color and tiles.shape != self.tiles_on_board[str(x) + " " + str(y + num)].shape:
+                                plays_shortlist.append(tiles)
+                                print(tiles)
+
+
+                    if self.tiles_on_board[str(x) + " " + str(y + num)].shape == tile.shape:
+                        for tiles in possible_tiles:
+                            if tiles.shape == tile.shape and tiles.color != self.tiles_on_board[str(x) + " " + str(y + num)].color:
+                                plays_shortlist.append(tiles)
+                                print(tiles)
+
+
+
+                    num += 1
+                print("\n")
+                print(*plays_shortlist, sep='\n')
+
+            # if str(x + 1) + " " + str(y) in self.tiles_on_board:
+            #     self.find_similar_tiles(self.tiles_on_board[str(x + 1) + " " + str(y)])
+            # if str(x) + " " + str(y - 1) in self.tiles_on_board:
+            #     self.find_similar_tiles(self.tiles_on_board[str(x) + " " + str(y - 1)])
+            # if str(x - 1) + " " + str(y) in self.tiles_on_board:
+            #     self.find_similar_tiles(self.tiles_on_board[str(x - 1) + " " + str(y)])
+
+
+
+
+
+
 
 
 
--- Test/boardTest.py
+++ Test/boardTest.py
@@ -4,26 +4,45 @@ from shape import *
 from color import *
 from tile import *
 
-def test_play():
-    board1 = Board()
-    tile = Tile(Shape.CLOVER, Color.BLUE)
-    coordinates = "1 2"
-    removed_from_hand = {coordinates: tile}
-    board1.add_to_board(removed_from_hand)
-    assert len(board1.tiles_on_board) == 1
+# def test_play():
+#     board1 = Board()
+#     tile = Tile(Shape.CLOVER, Color.BLUE)
+#     coordinates = "1 2"
+#     removed_from_hand = {coordinates: tile}
+#     board1.add_to_board(removed_from_hand)
+#     assert len(board1.tiles_on_board) == 1
+#
+# def test_coordinate_retrieval():
+#     board2 = Board()
+#     tile = Tile(Shape.CLOVER, Color.BLUE)
+#     board2.add_to_board({"3 8": tile, "3 7": tile})
+#     possible_plays = board2.find_spots()
+#     assert possible_plays == {(2, 7), (4, 7), (4, 8), (2, 8), (3, 9), (3, 6)}
 
-def test_coordinate_retrieval():
-    board2 = Board()
-    tile = Tile(Shape.CLOVER, Color.BLUE)
-    board2.add_to_board({"3 8": tile, "3 7": tile})
-    possible_plays = board2.find_legal_plays()
-    assert possible_plays == {(2, 7), (4, 7), (4, 8), (2, 8), (3, 9), (3, 6)}
+# def test_coordinate_retrieval_one_tile():
+#     board2 = Board()
+#     tile = Tile(Shape.CLOVER, Color.BLUE)
+#     board2.add_to_board({"3 8": tile})
+#     possible_plays = board2.find_spots()
+#     assert possible_plays == {(4, 8), (2, 8), (3, 9), (3, 7)}
+#
+# def test_find_similar_tiles():
+#     board1 = Board()
+#     tile = Tile(Shape.STAR, Color.YELLOW)
+#     similar_tiles_test = board1.find_similar_tiles(tile)
+#     assert len(similar_tiles_test) == 10
 
-def test_find_similar_tiles():
+def test_find_legal_plays():
     board1 = Board()
-    tile = Tile(Shape.STAR, Color.YELLOW)
-    similar_tiles_test = board1.find_similar_tiles(tile)
-    assert len(similar_tiles_test) == 10
+    tile1 = Tile(Shape.SUN, Color.BLUE)
+    tile2 = Tile(Shape.SUN, Color.GREEN)
+    tile3 = Tile(Shape.SQUARE, Color.BLUE)
+    tiles = {"0 0": tile1, "0 1": tile3}
+    board1.add_to_board(tiles)
+    board1.find_spots()
+    board1.find_legal_plays()
+
+
 
 
 
